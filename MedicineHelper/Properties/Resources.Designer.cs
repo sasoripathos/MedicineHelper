e6a759028110ed2eb6b8e40dad41f6139aff5985
Jerry <sasori.pan@mail.utoronto.ca>
2017-10-16 20:56:50 -0400

    Change the default namespace and assembly name

--- MedicineHelper/Properties/Resources.Designer.cs
+++ MedicineHelper/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace MedicineHelper.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ArthitisAndMeApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MedicineHelper.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
