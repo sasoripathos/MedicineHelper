c27daed360a5d4a56a9c5e27bbec1b0a8716e352
Jerry <sasori.pan@mail.utoronto.ca>
2017-11-05 13:31:45 -0500

    Add license notice in each file.

--- MedicineHelper/Form1.cs
+++ MedicineHelper/Form1.cs
@@ -1,4 +1,21 @@
-﻿
+﻿// Copyright Zhili (Jerry) Pan, October 2017
+// Distributed under the terms of the GNU General Public License.
+//
+// This file is part of MedicineHelper.
+//
+// MedicineHelper is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MedicineHelper is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
